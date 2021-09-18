939e5d14e4e0e0a0149024a758e20c8568a0c000
Paul Adams <paul@thecoderszone.com>
2021-09-18 12:58:33 +0000

    Sync LeetCode submission - Path Sum (cpp)

--- problems/path_sum/solution.cpp
+++ problems/path_sum/solution.cpp
@@ -14,7 +14,7 @@ public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if (!root)
             return false;
-        if (!root->left && !root->right && targetSum == root->val)
+        if (!root->left && !root->right && root->val == targetSum)
             return true;
         return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
     }
