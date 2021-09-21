13d1a0013484b6248c896073b5bed2a48bba294a
Paul Adams <paul@thecoderszone.com>
2021-09-21 12:42:35 +0000

    Sync LeetCode submission - Binary Tree Preorder Traversal (cpp)

--- problems/binary_tree_preorder_traversal/solution.cpp
+++ problems/binary_tree_preorder_traversal/solution.cpp
@@ -10,14 +10,21 @@
  * };
  */
 class Solution {
-    vector<int> result;
 public:
-    vector<int> preorderTraversal(TreeNode* root) {
-        if (!root)
-            return result;
-        result.push_back(root->val);
-        preorderTraversal(root->left);
-        preorderTraversal(root->right);
+    std::vector<int> preorderTraversal(TreeNode* root) {
+        std::vector<int> result;
+        std::stack<TreeNode*> stack;
+        if (root)
+            stack.push(root);
+        while (!stack.empty()) {
+            TreeNode* n = stack.top();
+            stack.pop();
+            result.push_back(n->val);
+            if (n->right)
+                stack.push(n->right);
+            if (n->left)
+                stack.push(n->left);
+        }
         return result;
     }
 };
