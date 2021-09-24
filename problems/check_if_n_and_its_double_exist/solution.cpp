fc5e184351d24cfd10a3129c6d1bdf2d97907b68
Paul Adams <paul@thecoderszone.com>
2021-09-24 14:10:51 +0000

    Sync LeetCode submission - Check If N and Its Double Exist (cpp)

--- problems/check_if_n_and_its_double_exist/solution.cpp
+++ problems/check_if_n_and_its_double_exist/solution.cpp
@@ -3,11 +3,11 @@ public:
     bool checkIfExist(vector<int>& arr) {
         unordered_set<int> s;
         for (int n : arr) {
-            if (s.find(n) != s.end())
+            if (s.find(n * 2) != s.end())
                 return true;
-            s.insert(n * 2);
-            if (n % 2 == 0)
-                s.insert(n / 2);
+            if (n % 2 == 0 && s.find(n / 2) != s.end())
+                return true;
+            s.insert(n);
         }
         return false;
     }
