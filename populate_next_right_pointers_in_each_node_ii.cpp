8afec0657fad65dcc764412cb33704ab5976a3ce
Paul Adams <paul@thecoderszone.com>
2021-11-09 10:31:35 +0000

    Create populate_next_right_pointers_in_each_node_ii.cpp

---
+++ populate_next_right_pointers_in_each_node_ii.cpp
@@ -0,0 +1,35 @@
+#include <queue>
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        std::queue<Node*> q;
+        if (root) q.push(root);
+        while (!q.empty()) {
+            int levelSize = q.size();
+            for (int i = 0; i < levelSize; i++) {
+                Node* n = q.front();
+                q.pop();
+                if (i != levelSize - 1) n->next = q.front();
+                if (n->left) q.push(n->left);
+                if (n->right) q.push(n->right);
+            }
+        }
+        return root;
+    }
+};
