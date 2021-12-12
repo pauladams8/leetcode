dc3c168fb8be7787b76cacab984e1e31d303d991
Paul Adams <paul@thecoderszone.com>
2021-12-12 17:32:27 +0000

    Add perfect squares problem

---
+++ perfect_squares.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <queue>
+
+class Solution {
+public:
+    int numSquares(int n) {
+        std::vector<int> squares;
+        std::queue<int> q;
+        for (int i = 1; i * i <= n; i++) {
+            int sq = i * i;
+            if (sq == n) return 1;
+            squares.push_back(sq);
+            q.push(sq);
+        }
+        int count = 2;
+        while (!q.empty()) {
+            int countSize = q.size();
+            for (int i = 0; i < countSize; i++) {
+                int sum = q.front();
+                q.pop();
+                for (int sq : squares) {
+                    int newSum = sum + sq;
+                    if (newSum == n) return count;
+                    q.push(newSum);
+                }
+            }
+            count++;
+        }
+        return -1;
+    }
+};
