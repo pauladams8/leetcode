ee2943aac37cb34aff86d5c44225b382e89f917d
Paul Adams <paul@thecoderszone.com>
2021-12-21 11:15:11 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation (cpp)

---
+++ problems/evaluate_reverse_polish_notation/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int evalRPN(std::vector<string>& tokens) {
+        std::stack<int> stack;
+        for (std::string token : tokens) {
+            if (token == "+" || token == "-" || token == "*" || token == "/") {
+                int right = stack.top();
+                stack.pop();
+                int left = stack.top();
+                stack.pop();
+                if (token == "+") stack.push(left + right);
+                if (token == "-") stack.push(left - right);
+                if (token == "*") stack.push(left * right);
+                if (token == "/") stack.push(left / right);
+            } else stack.push(std::stoi(token));
+        }
+        return stack.top();
+    }
+};
