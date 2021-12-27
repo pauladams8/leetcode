0e429b93c90fbd9a5f5d8ef19c49e9420fc60c4b
Paul Adams <paul@thecoderszone.com>
2021-12-27 19:15:22 +0000

    Add target sum problem

---
+++ target_sum.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    int findTargetSumWays(std::vector<int>& nums, int target) {
+        std::vector<std::unordered_map<int, int>> cache(nums.size());
+        return findTargetSumWays(nums, target, 0, cache);
+    }
+private:
+    int findTargetSumWays(std::vector<int>& nums, int target, int i, std::vector<std::unordered_map<int, int>>& cache) {
+        if (i >= nums.size())
+            return target == 0 ? 1 : 0;
+        if (cache[i].find(target) == cache[i].end())
+            cache[i][target] = findTargetSumWays(nums, target + nums[i], i + 1, cache) + findTargetSumWays(nums, target - nums[i], i + 1, cache);
+        return cache[i][target];
+    }
+};
