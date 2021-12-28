0412a2448b79c61a5c15764262c0012e3ddb31bd
Paul Adams <paul@thecoderszone.com>
2021-12-28 14:15:04 +0000

    Add implement stack using queues problem

---
+++ implement_stack_using_queues.cpp
@@ -0,0 +1,30 @@
+#include <queue>
+
+class MyStack {
+    std::queue<int> q;
+public:
+    MyStack() {}
+    
+    void push(int x) {
+        int size = q.size();
+        q.push(x);
+        for (int i = 0; i < size; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int val = q.front();
+        q.pop();
+        return val;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
