3497a9fa98f56a64ea0a808c48776b5f44e4f1b7
Paul Adams <paul@thecoderszone.com>
2021-12-31 18:46:00 +0000

    Sync LeetCode submission - Decode String (cpp)

--- problems/decode_string/solution.cpp
+++ problems/decode_string/solution.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
-    std::string decodeString(std::string s) {
+    std::string decodeString(std::string& s) {
+        int i = 0;
+        return decodeString(s, i);
+    }
+private:
+    std::string decodeString(std::string& s, int& i) {
         std::string result;
-        for (int i = 0; i < s.size(); i++) {
+        int k = 0;
+        for (; i < s.size() && s[i] != ']'; i++) {
             if (s[i] == '[') {
-                int kSize = 0;
-                for (int j = i - 1; j >= 0 && s[j] - '0' >= 0 && s[j] - '0' <= 9; j--) {
-                    result.pop_back();
-                    kSize++;
-                }
-                int k = std::stoi(s.substr(i - kSize, kSize));
-                std::cout << "k: " << k;
-                std::stack<int> stack;
-                stack.push(i);
-                int size = 0;
-                for (int j = i + 1; j < s.size() && !stack.empty(); j++) {
-                    if (s[j] == '[') stack.push(j);
-                    if (s[j] == ']') stack.pop();
-                    size++;
-                }
-                std::cout << ", str: " << s.substr(i + 1, size - 1) << '\n';
-                std::string decoded = decodeString(s.substr(i + 1, size - 1));
-                for (int j = 0; j < k; j++) result += decoded;
-                i += size;
-            } else result.push_back(s[i]);
+                i++;
+                std::string decoded = decodeString(s, i);
+                for (; k > 0; k--) result += decoded;
+            } else if (s[i] - '0' >= 0 && s[i] - '0' <= 9) k = k * 10 + (s[i] - '0');
+            else result += s[i];
         }
-        std::cout << "result: " << result << '\n';
         return result;
     }
 };
