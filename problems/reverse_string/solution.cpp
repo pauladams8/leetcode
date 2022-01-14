743b234fada3f248cdf056502ccfb16c30ed319c
Paul Adams <paul@thecoderszone.com>
2022-01-14 20:34:57 +0000

    Sync LeetCode submission - Reverse String (cpp)

---
+++ problems/reverse_string/solution.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    void reverseString(std::vector<char>& s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j)
+            std::swap(s[i++], s[j--]);
+    }
+};
