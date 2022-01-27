037c2e221636d2c3af07549b73b66011e975a252
Paul Adams <paul@thecoderszone.com>
2022-01-27 19:30:36 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- problems/fibonacci_number/solution.cpp
+++ problems/fibonacci_number/solution.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int fib(int n) {
-        static std::vector<int> cache(31, -1);
-        if (cache[n] != -1) return cache[n];
-        if (n <= 1) cache[n] = n;
-        else cache[n] = fib(n - 1) + fib(n - 2);
+        static std::vector<int> cache;
+        while (n >= cache.size()) {
+            int i = cache.size();
+            if (i <= 1) cache.push_back(i);
+            else cache.push_back(cache[i - 1] + cache[i - 2]);
+        }
         return cache[n];
     }
 };
