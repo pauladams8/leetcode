ade64b9fdcf3c9eef468485776095d016dabb3bf
Paul Adams <paul@thecoderszone.com>
2022-01-28 17:20:55 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (cpp)

--- problems/maximum_depth_of_binary_tree/solution.cpp
+++ problems/maximum_depth_of_binary_tree/solution.cpp
@@ -12,8 +12,7 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if (!root)
-            return 0;
-        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+        if (!root) return 0;
+        return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
     }
 };
