74499f6d01741d4549fc4514a7ad57a0e1e8f186
Paul Adams <paul@thecoderszone.com>
2022-01-30 21:28:31 +0000

    Refactor x^n

--- pow_x_n.cpp
+++ pow_x_n.cpp
@@ -3,7 +3,7 @@ public:
     double myPow(double x, int n) {
         double res = 1;
         while (n) {
-            if (n % 2 != 0)
+            if (n % 2)
                 res = n < 0 ? res / x : res * x;
             x *= x;
             n /= 2;
