c9495a6674acdfda63f8b391773bc9561fab3787
Paul Adams <paul@thecoderszone.com>
2022-02-11 12:02:42 +0000

    Sync LeetCode submission - Zigzag Conversion (cpp)

---
+++ problems/zigzag_conversion/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    std::string convert(std::string s, int numRows) {
+        if (numRows <= 1) return s;
+        const int period = 2 * numRows - 2;
+        std::string res;
+        for (int row = 0; row < numRows; row++)
+            for (int i = row; i < s.size(); i += period - 2 * (i % (numRows - 1)))
+                res += s[i];
+        return res;
+    }
+};
