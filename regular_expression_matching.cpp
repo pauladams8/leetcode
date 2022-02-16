9d2e54e705ae494d8c08faaeb6b6171533963fed
Paul Adams <paul@thecoderszone.com>
2022-02-16 12:24:44 +0000

    Add regular expression matching problem

---
+++ regular_expression_matching.cpp
@@ -0,0 +1,22 @@
+#include <string>
+
+class Solution {
+public:
+    bool isMatch(std::string& s, std::string& p) {
+        return isMatch(s, 0, p, 0);
+    }
+private:
+    bool isMatch(std::string& s, int sIndex, std::string& p, int pIndex) {
+        for (; pIndex < p.size(); pIndex++) {
+            if (pIndex + 1 < p.size() && p[pIndex + 1] == '*') {
+                while (sIndex < s.size() && p[pIndex] == '.' || s[sIndex] == p[pIndex])
+                    if (isMatch(s, sIndex++, p, pIndex + 2))
+                        return true;
+                pIndex++;
+            } else if (sIndex < s.size() && p[pIndex] == '.' || s[sIndex] == p[pIndex])
+                sIndex++;
+            else return false;
+        }
+        return sIndex == s.size() && pIndex == p.size();
+    }
+};
