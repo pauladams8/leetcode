d2517a4e7a7b2b98940573872ab27ab5a9cca1bc
Paul Adams <paul@thecoderszone.com>
2022-02-16 12:49:19 +0000

    Sync LeetCode submission - Swap Nodes in Pairs (cpp)

--- problems/swap_nodes_in_pairs/solution.cpp
+++ problems/swap_nodes_in_pairs/solution.cpp
@@ -11,16 +11,13 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        if (!head || !head->next) return head;
-        ListNode *first = head, *second = head->next, *third = second->next;
-        head = second;
-        second->next = first;
-        first->next = third;
-        for (ListNode* node = head->next; node && node->next && node->next->next; node = node->next->next) {
-            first = node->next, second = first->next, third = second->next;
-            node->next = second;
-            second->next = first;
-            first->next = third;
+        ListNode *node = head, **pp = &head;
+        while (node && node->next) {
+            *pp = node->next;
+            node->next = node->next->next;
+            (*pp)->next = node;
+            pp = &node->next;
+            node = node->next;
         }
         return head;
     }
