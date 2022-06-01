9155af5cf959045187832b832f5cddad05878def
Paul Adams <paul+github@thecoderszone.com>
2022-06-01 17:50:38 +0100

    Add rotate image problem

---
+++ rotate_image.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for (int i = 0; i < matrix.size() - 1; i++) {
+            for (int j = i; j < matrix.size() - i - 1; j++) {
+                swap(matrix[i][j], matrix[matrix.size() - 1 - j][i]);
+                swap(matrix[matrix.size() - 1 - j][i], matrix[matrix.size() - 1 - i][matrix.size() - 1 - j]);
+                swap(matrix[matrix.size() - 1 - i][matrix.size() - 1 - j], matrix[j][matrix.size() - 1 - i]);
+            }
+        }
+    }
+};
