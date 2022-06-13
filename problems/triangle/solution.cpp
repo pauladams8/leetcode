129b7a237b4643521d00f8e461b031a5d8403db9
Paul Adams <paul@thecoderszone.com>
2022-06-13 09:36:00 +0000

    Sync LeetCode submission - Triangle (cpp)

---
+++ problems/triangle/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<int> dp(triangle.size() + 1);
+        for (int row = triangle.size() - 1; row >= 0; row--)
+            for (int col = 0; col <= row; col++)
+                dp[col] = min(dp[col], dp[col + 1]) + triangle[row][col];
+        return dp[0];
+    }
+};
