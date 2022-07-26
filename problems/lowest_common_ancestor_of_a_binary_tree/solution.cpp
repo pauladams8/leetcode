967d9ebfc5093c7b43c616cfbeb067ea65a9e533
Paul Adams <paul@thecoderszone.com>
2022-07-26 11:52:59 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Tree (cpp)

--- problems/lowest_common_ancestor_of_a_binary_tree/solution.cpp
+++ problems/lowest_common_ancestor_of_a_binary_tree/solution.cpp
@@ -14,8 +14,7 @@ public:
             return nullptr;
         if (root == p || root == q)
             return root;
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        TreeNode *left = lowestCommonAncestor(root->left, p, q), *right = lowestCommonAncestor(root->right, p, q);
         if (left && right)
             return root;
         return left ?: right;
