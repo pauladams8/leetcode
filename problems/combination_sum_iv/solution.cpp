1c5e0ef16e6901a3f521b4b63741b59a1eccd36a
Paul Adams <paul@thecoderszone.com>
2022-08-10 16:57:32 +0000

    Sync LeetCode submission - Combination Sum IV (cpp)

--- problems/combination_sum_iv/solution.cpp
+++ problems/combination_sum_iv/solution.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> cache(target + 1, -1);
-        return combinationSum4(nums, target, cache);
-    }
-private:
-    int combinationSum4(vector<int>& nums, int target, vector<int>& cache) {
-        if (target < 0)
-            return 0;
-        if (target == 0)
-            return 1;
-        if (cache[target] != -1)
-            return cache[target];
-        cache[target] = 0;
-        for (int n : nums)
-            cache[target] += combinationSum4(nums, target - n, cache);
-        return cache[target];
+        vector<int> dp(target + 1);
+        dp[0] = 1;
+        for (int i = 0; i < target; i++)
+            for (int n : nums)
+                if (i + n <= target && dp[i + n] <= INT_MAX - dp[i])
+                    dp[i + n] += dp[i];
+        return dp[target];
     }
 };
