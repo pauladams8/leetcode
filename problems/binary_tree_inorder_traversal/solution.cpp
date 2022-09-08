5429d79c52ccc2d141717518c4ba988b7bb3faa3
Paul Adams <paul@thecoderszone.com>
2022-09-08 08:21:30 +0000

    Sync LeetCode submission - Binary Tree Inorder Traversal (cpp)

--- problems/binary_tree_inorder_traversal/solution.cpp
+++ problems/binary_tree_inorder_traversal/solution.cpp
@@ -12,18 +12,22 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> result;
         stack<TreeNode*> stack;
-        while (root || !stack.empty()) {
+        vector<int> res;
+        while (root) {
+            stack.push(root);
+            root = root->left;
+        }
+        while (!stack.empty()) {
+            root = stack.top();
+            stack.pop();
+            res.push_back(root->val);
+            root = root->right;
             while (root) {
                 stack.push(root);
                 root = root->left;
             }
-            root = stack.top();
-            stack.pop();
-            result.push_back(root->val);
-            root = root->right;
         }
-        return result;
+        return res;
     }
 };
