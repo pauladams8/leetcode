76641c9d5c13a2ae143602e3d91cf5fb920fafe9
Paul Adams <paul@thecoderszone.com>
2022-10-20 19:54:27 +0000

    Sync LeetCode submission - Integer to Roman (cpp)

---
+++ problems/integer_to_roman/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string roman;
+        vector<pair<string, int>> map = { { "M", 1000 }, { "CM", 900 }, { "D", 500 }, { "CD", 400 }, { "C", 100 }, { "XC", 90 }, { "L", 50 }, { "XL", 40 }, { "X", 10 }, { "IX", 9 }, { "V", 5 }, { "IV", 4 }, { "I", 1 } };
+        for (auto [symbol, value] : map)
+            while (num >= value)
+                roman += symbol, num -= value;
+        return roman;
+    }
+};
