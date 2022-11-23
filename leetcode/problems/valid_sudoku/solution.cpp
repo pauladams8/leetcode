665cd0ad786fc354c329b66ab0c327f30c20aa5e
Paul Adams <paul@thecoderszone.com>
2022-11-23 13:44:49 +0000

    Sync LeetCode submission - Valid Sudoku (cpp)

--- leetcode/problems/valid_sudoku/solution.cpp
+++ leetcode/problems/valid_sudoku/solution.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        for (int row = 0; row < 9; row++) {
-            vector<bool> seen(9);
-            for (int col = 0; col < 9; col++) {
-                if (board[row][col] == '.')
-                    continue;
-                if (seen[board[row][col] - '1'])
+        for (int i = 0; i < 9; i++) {
+            vector<bool> row(9), col(9), box(9);
+            for (int j = 0; j < 9; j++)
+                if (!isValid(board, row, i, j) || !isValid(board, col, j, i) || !isValid(board, box, i / 3 * 3 + j / 3, i % 3 * 3 + j % 3))
                     return false;
-                seen[board[row][col] - '1'] = true;
-            }
         }
-        for (int col = 0; col < 9; col++) {
-            vector<bool> seen(9);
-            for (int row = 0; row < 9; row++) {
-                if (board[row][col] == '.')
-                    continue;
-                if (seen[board[row][col] - '1'])
-                    return false;
-                seen[board[row][col] - '1'] = true;
-            }
-        }
-        for (int row = 0; row < 9; row += 3) {
-            for (int col = 0; col < 9; col += 3) {
-                vector<bool> seen(9);
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        if (board[row + i][col + j] == '.')
-                            continue;
-                        if (seen[board[row + i][col + j] - '1'])
-                            return false;
-                        seen[board[row + i][col + j] - '1'] = true;
-                    }
-                }
-            }
-        }
+        return true;
+    }
+private:
+    bool isValid(vector<vector<char>>& board, vector<bool>& seen, int row, int col) {
+        if (board[row][col] == '.')
+            return true;
+        if (seen[board[row][col] - '1'])
+            return false;
+        seen[board[row][col] - '1'] = true;
         return true;
     }
 };
