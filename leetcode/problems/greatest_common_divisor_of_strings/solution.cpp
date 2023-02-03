52193df6ee26d57397981186482f2d5844ef09fa
Paul Adams <paul@thecoderszone.com>
2023-02-03 00:38:34 +0000

    Sync LeetCode submission - Greatest Common Divisor of Strings (cpp)

---
+++ leetcode/problems/greatest_common_divisor_of_strings/solution.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    string gcdOfStrings(string s1, string s2) {
+        if (s1 + s2 != s2 + s1)
+            return "";
+        return s1.substr(0, gcd(s1.size(), s2.size()));
+    }
+};
