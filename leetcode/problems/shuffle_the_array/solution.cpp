7b05ad5731c16f897a638df07a12a6189eca1de5
Paul Adams <paul@thecoderszone.com>
2023-02-06 15:27:37 +0000

    Sync LeetCode submission - Shuffle the Array (cpp)

---
+++ leetcode/problems/shuffle_the_array/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans(nums.size());
+        for (int i = 0; i < n; i++)
+            ans[i * 2] = nums[i], ans[i * 2 + 1] = nums[i + n];
+        return ans;
+    }
+};
