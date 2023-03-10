044d32569853e57d4211371abc5ad3ea720b7c9d
Paul Adams <paul@thecoderszone.com>
2023-03-10 22:43:09 +0000

    Add advent of code day 6

---
+++ Advent of Code/2022/day6/part1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int firstUnique4(string& line) {
+    vector<int> seen(26, -1);
+    for (int start = 0, end = 0; end < line.size(); end++) {
+        start = max(start, seen[line[end] - 'a'] + 1);
+        if (end - start + 1 == 4)
+            return end + 1;
+        seen[line[end] - 'a'] = end;
+    }
+    return INT_MAX;
+}
+
+int main() {
+    string line;
+    while (cin >> line)
+        cout << firstUnique4(line) << '\n';
+}
