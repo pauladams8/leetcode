095eca2be17dc7dad4b43fb71bce9540c92f492b
Paul Adams <paul@thecoderszone.com>
2023-05-02 18:59:51 +0000

    Sync LeetCode submission - Sign of the Product of an Array (cpp)

---
+++ LeetCode/problems/sign_of_the_product_of_an_array/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int sign = 1;
+        for (int x : nums)
+            if (x == 0)
+                return 0;
+            else if (x < 0)
+                sign = -sign;
+        return sign;
+    }
+};
