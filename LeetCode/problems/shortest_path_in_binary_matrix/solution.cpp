04278864ae0491bd0b02bbdafb419ab977d7daba
Paul Adams <paul@thecoderszone.com>
2023-06-07 21:22:34 +0000

    Sync LeetCode submission - Shortest Path in Binary Matrix (cpp)

--- LeetCode/problems/shortest_path_in_binary_matrix/solution.cpp
+++ LeetCode/problems/shortest_path_in_binary_matrix/solution.cpp
@@ -1,30 +1,36 @@
 class Solution {
+    vector<pair<int, int>> dir = {
+        { -1, 0 },
+        { -1, 1 },
+        { 0, 1 },
+        { 1, 1 },
+        { 1, 0 },
+        { 1, -1 },
+        { 0, -1 },
+        { -1, -1 }
+    };
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        queue<pair<int, int>> q;
-        q.push({ 0, 0 });
-        for (int length = 1; !q.empty(); length++) {
-            for (int k = 0, n = q.size(); k < n; k++) {
+        int n = grid.size(), length = 1;
+        queue<pair<int, int>> q, next;
+        if (!grid[0][0])
+            q.push({ 0, 0 }), grid[0][0] = 1;
+        while (!q.empty()) {
+            while (!q.empty()) {
                 auto [i, j] = q.front();
                 q.pop();
-                if (i < 0 || i >= grid.size())
-                    continue;
-                if (j < 0 || j >= grid[i].size())
-                    continue;
-                if (grid[i][j] != 0)
-                    continue;
-                grid[i][j] = 1;
-                if (i == grid.size() - 1 && j == grid[i].size() - 1)
+                if (i == n - 1 && j == n - 1)
                     return length;
-                q.push({ i - 1, j });
-                q.push({ i - 1, j + 1 });
-                q.push({ i, j + 1 });
-                q.push({ i + 1, j + 1 });
-                q.push({ i + 1, j });
-                q.push({ i + 1, j - 1 });
-                q.push({ i, j - 1 });
-                q.push({ i - 1, j - 1 });
+                for (auto [di, dj] : dir) {
+                    int ii = i + di, jj = j + dj;
+                    if (ii < 0 || ii >= n || jj < 0 || jj >= n || grid[ii][jj])
+                        continue;
+                    next.push({ ii, jj });
+                    grid[ii][jj] = 1;
+                }
             }
+            length++;
+            swap(q, next);
         }
         return -1;
     }
