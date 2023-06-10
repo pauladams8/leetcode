14c70a732650ae86a14f491571e7c9852951386f
Paul Adams <paul@thecoderszone.com>
2023-06-10 22:01:43 +0000

    Sync LeetCode submission - Find Smallest Letter Greater Than Target (cpp)

---
+++ LeetCode/problems/find_smallest_letter_greater_than_target/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int left = 0, right = letters.size() - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (letters[mid] > target)
+                right = mid;
+            else left = mid + 1;
+        }
+        return letters[left] > target ? letters[left] : letters[0];
+    }
+};
