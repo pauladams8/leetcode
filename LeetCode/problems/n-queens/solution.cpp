655c80aa4c6e75a0a7e6eebd7e747877f18e0f59
Paul Adams <paul@thecoderszone.com>
2023-09-09 15:01:25 +0000

    Sync LeetCode submission - N-Queens (cpp)

--- LeetCode/problems/n-queens/solution.cpp
+++ LeetCode/problems/n-queens/solution.cpp
@@ -2,57 +2,37 @@ class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
         vector<string> board(n, string(n, '.'));
-        vector<vector<bool>> blocked(n, vector<bool>(n));
-        vector<vector<bool>> used(n, vector<bool>(n));
-        vector<vector<string>> boards;
-        solveNQueens(n, board, blocked, used, boards);
-        return boards;
+        vector<vector<string>> ans;
+        solve(board, ans, 0);
+        return ans;
     }
 private:
-    void solveNQueens(int n, vector<string>& board, vector<vector<bool>>& blocked, vector<vector<bool>>& prevUsed, vector<vector<string>>& boards) {
-        if (n == 0) {
-            boards.push_back(board);
+    void solve(vector<string>& board, vector<vector<string>>& ans, int row) {
+        int n = board.size();
+        if (row == n) {
+            ans.push_back(board);
             return;
         }
-        vector<vector<bool>> used(board.size(), vector<bool>(board.size()));
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[i].size(); j++) {
-                if (blocked[i][j])
-                    continue;
-                prevUsed[i][j] = true;
-                if (used[i][j])
-                    continue;
-                board[i][j] = 'Q';
-                vector<pair<int, int>> directions = {
-                    { -1, 0 },
-                    { -1, 1 },
-                    { 0, 1 },
-                    { 1, 1 },
-                    { 1, 0 },
-                    { 1, -1 },
-                    { 0, -1 },
-                    { -1, -1 }
-                };
-                stack<pair<int, int>> stack;
-                for (auto [di, dj] : directions) {
-                    int ii = i, jj = j;
-                    while (ii >= 0 && jj >= 0 && ii < board.size() && jj < board[ii].size()) {
-                        if (!blocked[ii][jj]) {
-                            blocked[ii][jj] = true;
-                            stack.push({ ii, jj });
-                        }
-                        ii += di;
-                        jj += dj;
-                    }
-                }
-                solveNQueens(n - 1, board, blocked, used, boards);
-                board[i][j] = '.';
-                while (!stack.empty()) {
-                    auto [ii, jj] = stack.top();
-                    stack.pop();
-                    blocked[ii][jj] = false;
-                }
-            }
+        for (int col = 0; col < n; col++) {
+            if (!isValid(board, row, col))
+                continue;
+            board[row][col] = 'Q';
+            solve(board, ans, row + 1);
+            board[row][col] = '.';
         }
+    }
+private:
+    bool isValid(vector<string>& board, int row, int col) {
+        int n = board.size();
+        for (int i = 0; i < n; i++)
+            if (board[row][i] == 'Q' || board[i][col] == 'Q')
+                return false;
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 'Q')
+                return false;
+        for (int i = row, j = col; i >= 0 && j < n; i--, j++)
+            if (board[i][j] == 'Q')
+                return false;
+        return true;
     }
 };
