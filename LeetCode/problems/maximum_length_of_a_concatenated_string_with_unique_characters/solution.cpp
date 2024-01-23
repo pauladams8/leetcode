1c15c0c776b1b98c6279f5c978ec17d526d3dd97
Paul Adams <paul@thecoderszone.com>
2024-01-23 10:47:02 +0000

    Sync LeetCode submission - Maximum Length of a Concatenated String with Unique Characters (cpp)

--- LeetCode/problems/maximum_length_of_a_concatenated_string_with_unique_characters/solution.cpp
+++ LeetCode/problems/maximum_length_of_a_concatenated_string_with_unique_characters/solution.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     int maxLength(vector<string>& arr) {
         vector<bool> contains(26);
-        return maxLength(arr, contains, 0);
+        return dfs(arr, 0, contains);
     }
 private:
-    int maxLength(vector<string>& arr, vector<bool>& contains, int start) {
-        if (start >= arr.size())
-            return 0;
-        int ans = 0;
-        for (int i = start; i < arr.size(); i++) {
-            vector<bool> newContains(contains);
-            if (isFresh(arr[i], newContains))
-                ans = max(ans, maxLength(arr, newContains, i + 1) + (int) arr[i].size());
+    int dfs(vector<string>& arr, int start, vector<bool>& contains) {
+        int n = arr.size(), ans = 0;
+        for (int i = start; i < n; i++) {
+            vector<bool> new_contains(contains);
+            if (ok(arr[i], new_contains)) {
+                ans = max(ans, dfs(arr, i + 1, new_contains)
+                          + (int)arr[i].size());
+            }
         }
         return ans;
     }
-private:
-    bool isFresh(string& s, vector<bool>& contains) {
+
+    bool ok(string& s, vector<bool>& contains) {
         for (char c : s) {
-            if (contains[c - 'a'])
+            if (contains[c - 'a']) {
                 return false;
+            }
             contains[c - 'a'] = true;
         }
         return true;
