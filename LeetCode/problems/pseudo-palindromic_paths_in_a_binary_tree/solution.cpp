104c514deb0d58c4c90e297be264f64e5875ade6
Paul Adams <paul@thecoderszone.com>
2024-01-25 00:04:08 +0000

    Sync LeetCode submission - Pseudo-Palindromic Paths in a Binary Tree (cpp)

--- LeetCode/problems/pseudo-palindromic_paths_in_a_binary_tree/solution.cpp
+++ LeetCode/problems/pseudo-palindromic_paths_in_a_binary_tree/solution.cpp
@@ -12,26 +12,15 @@
 class Solution {
 public:
     int pseudoPalindromicPaths(TreeNode* root) {
-        vector<int> freq(10);
-        return pseudoPalindromicPaths(root, freq);
+        return dfs(root, 0);
     }
 private:
-    int pseudoPalindromicPaths(TreeNode* root, vector<int>& freq) {
-        if (!root) return 0;
-        freq[root->val]++;
-        if (!root->left && !root->right) {
-            bool odd = false;
-            for (int i = 1; i <= 9; i++)
-                if (freq[i] % 2)
-                    if (odd) return back(root, freq, 0);
-                    else odd = true;
-            return back(root, freq, 1);
-        }
-        return back(root, freq, pseudoPalindromicPaths(root->left, freq) + pseudoPalindromicPaths(root->right, freq));
-    }
-    
-    int back(TreeNode* root, vector<int>& freq, int ret) {
-        freq[root->val]--;
-        return ret;
+    int dfs(TreeNode *root, int freq) {
+        if (!root)
+            return 0;
+        freq ^= 1 << root->val;
+        if (!root->left && !root->right)
+            return (freq & (freq - 1)) == 0;
+        return dfs(root->left, freq) + dfs(root->right, freq);
     }
 };
