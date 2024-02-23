bbbc9df66dd54cbb47457159535595032a227ab9
Paul Adams <paul@thecoderszone.com>
2024-02-23 21:50:44 +0000

    Sync LeetCode submission - Find the Town Judge (cpp)

--- LeetCode/problems/find_the_town_judge/solution.cpp
+++ LeetCode/problems/find_the_town_judge/solution.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> trusts(n + 1), trustedBy(n + 1);
-        for (auto edge : trust)
-            trusts[edge[0]]++, trustedBy[edge[1]]++;
-        for (int i = 1; i <= n; i++)
-            if (trusts[i] == 0 && trustedBy[i] == n - 1)
+        vector<int> trusts(n + 1), trusted_by(n + 1);
+        for (auto& edge : trust) {
+            trusts[edge[0]]++;
+            trusted_by[edge[1]]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (trusts[i] == 0 && trusted_by[i] == n - 1) {
                 return i;
+            }
+        }
         return -1;
     }
 };
